bd2401c0f47e87ec5bcb49e51ac22b68a2c63be9
SmokeySam <smokeydevelopment@gmail.com>
2021-04-30 15:04:16 +0000

    Bind Data from DB to accordion

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/recipes")
 def get_recipes():
-    recipes = mongo.db.recipes.find()
+    recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
 
